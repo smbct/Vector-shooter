ecc208a7f45891a7037960937b49e2f0e6a3ef60
smbct <smbct.dev@yahoo.com>
2016-08-16 14:25:46 +0200

    particle manager

---
+++ include/Utils.hpp
@@ -0,0 +1,20 @@
+/*!
+* \file Utils.hpp
+* \brief Utilities functions definitions
+* \author smbct
+* \date 08.17.2016
+*/
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+class Utils {
+
+    public:
+        static void randInit();
+        static double randRange(double min, double max);
+        static int randRange(int min, int max);
+
+};
+
+#endif /* UTILS_HPP */
---
+++ src/Particle.cpp
@@ -0,0 +1,18 @@
+/*!
+* \file Particle.cpp
+* \brief class implementation for particles
+* \author smbct
+* \date 08.17.2016
+*/
+
+#include "Particle.hpp"
+
+/*----------------------------------------------------------------------------*/
+Particle::Particle() {
+
+}
+
+/*----------------------------------------------------------------------------*/
+void Particle::update(double elapsedTime) {
+
+}
